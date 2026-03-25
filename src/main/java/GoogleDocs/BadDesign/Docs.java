fe55a68fb34e771f8d9fc994d4e975f770b68f16
achyut <achyutchaudhary47@gmail.com>
2026-03-25 20:27:45 +0530

    bad design

---
+++ src/main/java/GoogleDocs/BadDesign/Docs.java
@@ -0,0 +1,17 @@
+package GoogleDocs.BadDesign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Docs {
+    private List<String>documentList;
+    private String renderedDocument;
+
+    public Docs() {
+        documentList = new ArrayList<>();
+        renderedDocument = "";
+    }
+
+    //add a
+
+}
